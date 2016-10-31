79f80305c33cb9492dbaf47a2ecdb23b08eddd21
Marcelo de Souza <mdesouza@samsao.co>
2016-10-31 10:46:45 -0400

    Added comments and method to chunk data

--- Bluetooth/BTService.swift
+++ Bluetooth/BTService.swift
@@ -10,15 +10,21 @@ import Foundation
 import CoreBluetooth
 
 /* Services & Characteristics UUIDs */
+
 let BLEServiceUUID = CBUUID(string: "6E400001-B5A3-F393-E0A9-E50E24DCCA9E")
 let WriteCharUUID = CBUUID(string: "6E400002-B5A3-F393-E0A9-E50E24DCCA9E")
 let ReadCharUUID = CBUUID(string: "b4520103-a308-4e56-8a52-536c2ad07147")
 let BLEServiceChangedStatusNotification = "kBLEServiceChangedStatusNotification"
 
 class BTService: NSObject, CBPeripheralDelegate {
+
+	// MARK: Properties
+
 	var peripheral: CBPeripheral?
 	var writeCharacteristic: CBCharacteristic?
 	var readCharacteristic: CBCharacteristic?
+
+	// MARK: Initializers
 
 	init(initWithPeripheral peripheral: CBPeripheral) {
 		super.init()
@@ -30,16 +36,39 @@ class BTService: NSObject, CBPeripheralDelegate {
 		self.reset()
 	}
 
+	// MARK: Bluetooth methods
+
+	/**
+     Discovery devices
+     */
 	func startDiscoveringServices() {
 		self.peripheral?.discoverServices([BLEServiceUUID])
 	}
 
+	/**
+     Reset bluetooth configuration
+     */
 	func reset() {
 		if peripheral != nil {
 			peripheral = nil
 		}
 		// Deallocating therefore send notification
 		self.sendBTServiceNotificationWithIsBluetoothConnected(false)
+	}
+
+	/**
+     Write to the device
+     - parameter value: value
+     */
+	func writePosition(value: UInt8) {
+		// See if characteristic has been discovered before writing to it
+		if let writeCharacteristic = self.writeCharacteristic {
+			// Need a mutable var to pass to writeValue function
+			var charValue = value
+			for data in chunkData(NSData(bytes: &charValue, length: sizeof(UInt8))) {
+				self.peripheral?.writeValue(data, forCharacteristic: writeCharacteristic, type: CBCharacteristicWriteType.WithoutResponse)
+			}
+		}
 	}
 
 	// Mark: - CBPeripheralDelegate
@@ -89,14 +118,21 @@ class BTService: NSObject, CBPeripheralDelegate {
 
 	// Mark: - Private
 
-	func writePosition(value: UInt8) {
-		// See if characteristic has been discovered before writing to it
-		if let writeCharacteristic = self.writeCharacteristic {
-			// Need a mutable var to pass to writeValue function
-			var charValue = value
-			let data = NSData(bytes: &charValue, length: sizeof(UInt8))
-			self.peripheral?.writeValue(data, forCharacteristic: writeCharacteristic, type: CBCharacteristicWriteType.WithoutResponse)
-		}
+	/**
+     Chunk data to be able to fit on the packet
+     - parameter data: full data packet
+     - returns: array of data
+     */
+	private func chunkData(data: NSData) -> [NSData] {
+		let length = data.length
+		var offset = 0
+		var array: [NSData] = []
+		repeat {
+			let thisChunkSize = ((length - offset) > 20) ? 20 : (length - offset)
+			array.append(data.subdataWithRange(NSRange(location: offset, length: thisChunkSize)))
+			offset += thisChunkSize
+		} while (offset < length)
+		return array
 	}
 
 	func sendBTServiceNotificationWithIsBluetoothConnected(isBluetoothConnected: Bool) {
